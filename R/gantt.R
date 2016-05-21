29c68b82135d3f37afade78bcffa31a63808a84c
Dan Kelley <kelley.dan@gmail.com>
2016-05-21 12:28:02 -0300

    ganttAddTask doc improved

--- R/gantt.R
+++ R/gantt.R
@@ -577,11 +577,26 @@ read.gantt <- function(file, debug=FALSE)
 #' to be integers); see \dQuote{Examples}.
 #'
 #' @examples
-#' library(plan)
-#' data(gantt)
-#' # Add a sort of section divider to a gantt diagram.
-#' g <- ganttAddTask(gantt, "Writing", key=4.5)
-#' plot(g, ylabels=list(font=c(1,1,1,1,2)))
+#' library("plan")
+#' g <- new("gantt")
+#' g <- ganttAddTask(g, "Courses") # no times, so a heading
+#' g <- ganttAddTask(g, "Physical Oceanography", "2016-09-03", "2016-12-05")
+#' g <- ganttAddTask(g, "Chemistry Oceanography", "2016-09-03", "2016-12-05")
+#' g <- ganttAddTask(g, "Fluid Dynamics", "2016-09-03", "2016-12-05")
+#' g <- ganttAddTask(g, "Biological Oceanography", "2017-01-03", "2017-04-05")
+#' g <- ganttAddTask(g, "Geological Oceanography", "2017-01-03", "2017-04-05")
+#' g <- ganttAddTask(g, "Time-series Analysis", "2017-01-03", "2017-04-05")
+#' g <- ganttAddTask(g, "Research") # no times, so a heading
+#' g <- ganttAddTask(g, "Literature review", "2016-09-03", "2017-04-05")
+#' g <- ganttAddTask(g, "Develop analysis skills", "2016-09-03", "2017-08-01")
+#' g <- ganttAddTask(g, "Thesis work", "2017-01-01", "2018-04-01")
+#' g <- ganttAddTask(g, "Defend thesis proposal", "2017-05-01", "2017-06-01")
+#' g <- ganttAddTask(g, "Write papers & thesis", "2017-05-01", "2018-04-01")
+#' g <- ganttAddTask(g, "Defend thesis", "2018-05-01", "2018-05-15")
+#' # Set 'font' for bold-faced headings
+#' font <- ifelse(is.na(g[["start"]]), 2, 1)
+#' plot(g, ylabel=list(font=font))
+#'
 #' @family things related to \code{gantt} data
 ganttAddTask <- function(g, description="", start=NA, end=NA, done=0, neededBy=NA, key)
 {
--- inst/doc/plan.R
+++ inst/doc/plan.R
@@ -20,7 +20,7 @@ g <- ganttAddTask(g, "Chemistry Oceanography", "2016-09-03", "2016-12-05")
 g <- ganttAddTask(g, "Fluid Dynamics", "2016-09-03", "2016-12-05")
 g <- ganttAddTask(g, "Biological Oceanography", "2017-01-03", "2017-04-05")
 g <- ganttAddTask(g, "Geological Oceanography", "2017-01-03", "2017-04-05")
-g <- ganttAddTask(g, "Geophysical Fluid Dynamics", "2017-01-03", "2017-04-05")
+g <- ganttAddTask(g, "Time-series Analysis", "2017-01-03", "2017-04-05")
 g <- ganttAddTask(g, "Research") # no times, so a heading
 g <- ganttAddTask(g, "Literature review", "2016-09-03", "2017-04-05")
 g <- ganttAddTask(g, "Develop analysis skills", "2016-09-03", "2017-08-01")
@@ -28,7 +28,7 @@ g <- ganttAddTask(g, "Thesis work", "2017-01-01", "2018-04-01")
 g <- ganttAddTask(g, "Defend thesis proposal", "2017-05-01", "2017-06-01")
 g <- ganttAddTask(g, "Write papers & thesis", "2017-05-01", "2018-04-01")
 g <- ganttAddTask(g, "Defend thesis", "2018-05-01", "2018-05-15")
-## Set 'font' to highlight headings
+# Set 'font' for bold-faced headings
 font <- ifelse(is.na(g[["start"]]), 2, 1)
 plot(g, ylabel=list(font=font))
 
